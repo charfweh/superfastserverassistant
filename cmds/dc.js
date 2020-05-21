1369bb27f8521e81599c29007115b9bb1b80b752
charfweh <affan.ismail2016@gmail.com>
2020-05-21 16:12:00 +0530

    Delete channel mod updated

--- cmds/dc.js
+++ cmds/dc.js
@@ -3,15 +3,12 @@ module.exports.run = async (bot,message,args)=>{
     if(args){
         if(args.length > 5) return message.channel.send("Channel arguments should be less than 5")
         args.forEach(a=>{
+            //can delete mult+iple channels
+            //returns array of channels to be deleted
             let dc = message.guild.channels.filter(c=>a.includes(c.name))
             if(dc.array().length == 0) return message.channel.send(`No such channel/channels`)
-
             dc.forEach(dc=>{
-                try{
-                    message.guild.channels.get(dc.id).delete().then(message.channel.send(`Channel with name **${dc.name}** deleted`))
-                }catch(err){
-                    message.channel.send("Sorry, we ran into error")
-                }
+                message.guild.channels.get(dc.id).delete().then(message.channel.send(`Channel deleted`))
             })
         })
     }
