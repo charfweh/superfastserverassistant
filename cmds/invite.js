6115b4061cbbd425f5216bbaeabd859586f3283a
charfweh <affan.ismail2016@gmail.com>
2020-05-21 16:27:19 +0530

    invite module added

--- cmds/invite.js
+++ cmds/invite.js
@@ -1,6 +1,6 @@
 module.exports.run = async (bot,message,args)=>{
     await message.channel.send("You've got a dm")
-    await message.author.send("https://discord.com/api/oauth2/authorize?client_id=712589018532937738&permissions=8&scope=bot")
+    await message.author.send("Invite me~\nhttps://discord.com/api/oauth2/authorize?client_id=712589018532937738&permissions=8&scope=bot")
 }
 module.exports.help={
     "name":"i"
