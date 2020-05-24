04c330b5b67f5c605a8d7b9b6a092b597340b7a7
charfweh <affan.ismail2016@gmail.com>
2020-05-24 16:25:37 +0530

    CreateChannel mod updated

--- cmds/cc.js
+++ cmds/cc.js
@@ -1,14 +1,18 @@
 module.exports.run = async (bot,message,args)=>{
     if(!args[0]) return message.channel.send("Specify args")
     if(args.length > 5) return message.channel.send("Channel arguments should be less than 5").then(msg=>msg.delete(2000))
-    if(args.length < 5){
-        args.forEach(a=>{
-            let c = message.guild.channels.find(c=>c.name == a)
-            if(c) return message.channel.send(`Cannot create duplicate channels, **${c}** already exists`)
-            message.guild.createChannel(a,'text').then(()=>{
-                message.channel.send(`Channel created with name **${a}**`)
-            })
+    if(args.length <= 5){
+        let uniarr = []
+        for(i = 0 ; i < args.length; i++){
+            if(uniarr.indexOf(args[i]) === -1){
+                uniarr.push(args[i])
+            }
+        }
+        console.log(uniarr)
+        uniarr.forEach(c=>{
+            message.guild.createChannel(c,'text').then(message.channel.send("Channel created!"))
         })
+        
     }
     
 }
