3ee655e980eb8938366621c07f1ec7e13383895c
charfweh <affan.ismail2016@gmail.com>
2020-05-31 16:37:36 +0530

    cmd mods updated, crole and drole added

--- cmds/crole.js
+++ cmds/crole.js
@@ -1,18 +1,41 @@
 module.exports.run = async (bot,message,args)=>{
-    if(!args) return message.channel.send("No arguments provided")
-    if(args){
-        if(args.length > 5) return message.channel.send("Roles argument should be less than 5")
-        args.forEach(r=>{
-            let roles = message.guild.roles.find(role=>role.name == r)
-            if(roles) return message.channel.send(`Cannot create duplicate role, **${r}** role already exists`)
-            message.guild.createRole({
-                name: r,
-                color:"RANDOM",
-                mentionable: true
-                }).then(message.channel.send(`Role created **${r}**`))
-        })
+    if(!args[0]) return message.channel.send("Specify args")
+
+    if(args.length > 5) return message.channel.send("Roles arguments should be less than 5").then(msg=>msg.delete(2000))
+    if(args.length <= 5){
+      console.log("Args"+args)
+        args[0]= args[0].replace("cr","")
+        let uniarr = []
+        let f_arr = args.map(item=>item.trim())
+        console.log("whitespace arr"+f_arr)
+        for(i = 0 ; i < f_arr.length; i++){
+            if(uniarr.indexOf(f_arr[i]) === -1){
+                uniarr.push(f_arr[i])
+            }
+        }
+        console.log(uniarr)
+        try {
+          await uniarr.forEach((item, i) => {
+              let role = message.guild.roles.find(r=>r.name == item)
+              if(role){
+                message.channel.send("Cannot create duplicate roles, "+role+" already exists")
+              }
+              if(!role){
+                message.guild.createRole({
+                name:item,
+                color:"RANDOM"
+              })
+            }
+          });
+        } catch (e) {
+            message.channel.send("Ran into error, error logs will be sent to developer")
+            console.log(e)
+        }
+
+
     }
+
 }
-module.exports.help={
-    "name":"cr"
+module.exports.help = {
+    name:"cr"
 }
---
+++ cmds/drole.js
@@ -0,0 +1,28 @@
+module.exports.run = async (bot,message,args)=>{
+    if(!args) return message.channel.send("Specify args")
+    if(args){
+        if(args.length > 5) return message.channel.send("Channel arguments should be less than 5")
+        args[0] = args[0].replace("dr","")
+        let f_role = args.map(role=>role.trim())
+        console.log(f_role)
+        for(i = 0 ; i < f_role.length; i++){
+          try{
+            let role = message.guild.roles.find(rn => rn.name === f_role[i])
+            if(!role) message.channel.send(`Role doesnt exists under name **${f_role[i]}**`)
+            if(role){
+              await message.channel.send(`Deleting role with name **${role.name}**`)
+              await role.delete().then(()=>{
+                message.channel.send("Role deleted successfully")
+              }).catch(()=>{
+                message.channel.send("Couldnt delete role, Mising permission")
+              })
+            }
+          } catch(err){
+            message.channel.send("Ran into an error, please contact developer, error logs will be sent to developer")
+          }
+        }
+    }
+}
+module.exports.help={
+    "name":"dr"
+}
