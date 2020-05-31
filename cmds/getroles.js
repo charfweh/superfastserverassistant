fc211abe04c8f5e1cf6f8e71805182cec9582562
charfweh <affan.ismail2016@gmail.com>
2020-05-31 17:01:03 +0530

    CMd modes updated getroles added

---
+++ cmds/getroles.js
@@ -0,0 +1,17 @@
+const Discord = require('../../backup/node_modules/discord.js')
+module.exports.run = async (bot,message,args)=>{
+    message.guild.roles.forEach((item, i) => {
+      message.channel.send(`Name ${item.name} Editable ${item.editable}`)
+    }); 
+    // const embed = new Discord.RichEmbed()
+    // let roles = []
+    // embed.setTitle("Role info")
+    // embed.setThumbnail(message.author.avatarURL)
+    // embed.addField("Role",roles.toString())
+    // embed.setTimestamp();
+    // message.channel.send(embed)
+
+}
+module.exports.help = {
+    name:"getroles"
+}
